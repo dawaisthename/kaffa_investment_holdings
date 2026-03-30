3d8a3fdcb46dc7abc01b9e11a2963ed15a63e24d
dawaisthename <seifudawde@gmail.com>
2026-03-30 10:45:10 +0300

    feat:make the footer and header hidden

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer"; // Assume standard footer markup
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -16,41 +16,53 @@ import ScrollToTop from "./components/ScrollToTop";
 import NotFound from "./components/NotFound";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 
+function AppContent() {
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith("/admin");
+
+  return (
+    <div className="min-h-screen bg-kaffa-dark flex flex-col">
+      <ScrollToTop />
+
+      {!isAdmin && <Header />}
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/investment" element={<Investment />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/news/:id" element={<NewsDetail />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/careers/:id" element={<JobDetail />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/privacy"
+            element={<LegalPage data={legalContent.privacy} />}
+          />
+          <Route
+            path="/terms"
+            element={<LegalPage data={legalContent.terms} />}
+          />
+          <Route
+            path="/cookies"
+            element={<LegalPage data={legalContent.cookies} />}
+          />
+          <Route path="/admin-portal-kaffa" element={<AdminDashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+
+      {!isAdmin && <Footer />}
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-kaffa-dark flex flex-col">
-        <ScrollToTop />
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/investment" element={<Investment />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/news/:id" element={<NewsDetail />} />
-            <Route path="/careers" element={<Careers />} />
-            <Route path="/careers/:id" element={<JobDetail />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route
-              path="/privacy"
-              element={<LegalPage data={legalContent.privacy} />}
-            />
-            <Route
-              path="/terms"
-              element={<LegalPage data={legalContent.terms} />}
-            />
-            <Route
-              path="/cookies"
-              element={<LegalPage data={legalContent.cookies} />}
-            />
-            <Route path="/admin-portal-kaffa" element={<AdminDashboard />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppContent />
     </BrowserRouter>
   );
 }
